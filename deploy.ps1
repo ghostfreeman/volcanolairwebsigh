468e25908b95993ced51229523f9463bf748b93f
Cameron Kilgore <ghostfreeman@gmail.com>
2023-04-02 00:15:34 -0500

    Fuck it just use Measure-Command

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,5 @@
-$StartSec = (Get-Date).Second
 echo "Building site..."
 hugo
 echo "Uploading to DreamHost"
 scp -rp public/* dh_cpk8sq@alfalfa.dreamhost.com:volcanolair.co
-$EndSec = (Get-Date).Second
-Write-Host "Complete. Took $($EndSec - $StartSec) second(s) to complete."
+Write-Host "Complete."
